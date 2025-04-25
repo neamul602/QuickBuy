535fd888c44155ea4f3f4e63128792d4d7149f9c
neamul haq <neamul.haq@brainstation-23.com>
2025-04-25 15:08:08 +0600

    Creating Necessary Models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from product.validators import validate_file_size
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="products")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-id',]
+
+    def __str__(self):
+        return self.name
+    
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(
+        upload_to="products/images/", validators=[validate_file_size])
