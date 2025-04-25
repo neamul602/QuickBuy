66684169c3877d7ea58cee0e851dcdae72a06408
neamul haq <neamul.haq@brainstation-23.com>
2025-04-25 16:50:16 +0600

    Debug Toolbar Setup

--- quick_buy/settings.py
+++ quick_buy/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'product',
     'users',
     'order',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -74,6 +76,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'quick_buy.wsgi.application'
 
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
